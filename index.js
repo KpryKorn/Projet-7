f277af1f015e14bbd96f3ab55dcf393dd09d581c
KpryKorn <roffinisacha@gmail.com>
2024-05-16 15:24:14 +0200

    feat: add search function, dom elements

--- index.js
+++ index.js
@@ -5,9 +5,34 @@ import {
   getUniqueAppliances,
   getUniqueUtensils,
 } from "./scripts/utils/filters.js";
+import { searchByQuery } from "./scripts/utils/search.js";
 
 function getInitialRecipes() {
   return recipes;
+}
+
+function search() {
+  const searchBar = document.querySelector("#searchbar");
+  const searchInput = searchBar.querySelector("input");
+  const searchClose = searchBar.querySelector("svg");
+
+  searchClose.style.display = searchInput.value ? "block" : "none";
+  // à chaque nouvel input, lance la recherche en fonction de la query
+  searchInput.addEventListener("input", (e) => {
+    const searchValue = e.target.value;
+    console.log(
+      "Début du filtrage des recettes selon la recherche :",
+      searchValue
+    );
+    searchClose.style.display = searchValue ? "block" : "none";
+    searchByQuery(recipes, searchValue);
+  });
+
+  searchClose.addEventListener("click", () => {
+    searchInput.value = "";
+    searchClose.style.display = "none";
+    displayRecipes(getInitialRecipes());
+  });
 }
 
 function init() {
@@ -16,6 +41,8 @@ function init() {
   getUniqueAppliances(getInitialRecipes());
   getUniqueUtensils(getInitialRecipes());
   displayRecipes(getInitialRecipes());
+
+  search();
 }
 
 // Appel de la fonction d'initialisation après chargement du DOM
