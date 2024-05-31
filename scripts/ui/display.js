1048bea6d0cd5adcf90696ab85e5e65593b6b2ad
KpryKorn <roffinisacha@gmail.com>
2024-05-31 14:52:39 +0200

    fix: sert désormais uniquement à afficher les recettes

--- scripts/ui/display.js
+++ scripts/ui/display.js
@@ -1,32 +1,14 @@
+import { createRecipeCard } from "./recipesCard.js";
 import { displayCount } from "./recipesCount.js";
 
 function displayRecipes(recipes) {
-  const template = document.querySelector("#recipeCard");
   const container = document.querySelector("#recipes");
   container.innerHTML = "";
   displayCount(recipes);
 
   if (recipes.length > 0) {
     recipes.forEach((recipe) => {
-      const clone = template.content.cloneNode(true);
-      clone.querySelector(
-        ".card-img-top"
-      ).src = `./assets/img/Recettes/Resized/${recipe.image}`;
-      clone.querySelector(".card-img-top").alt = recipe.name;
-      clone.querySelector(".badge").textContent = `${recipe.time} min`;
-      clone.querySelector(".card-title").textContent = recipe.name;
-      clone.querySelector(".card-text").textContent = recipe.description;
-
-      // Afficher les ingrédients de la recette dans le DOM de la carte
-      const ingredientsListElement = clone.querySelector("#ingredientsList");
-      recipe.ingredients.forEach((ingredient) => {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `${ingredient.ingredient}: ${
-          ingredient.quantity || ""
-        } ${ingredient.unit || ""}`;
-        ingredientsListElement.appendChild(listItem);
-      });
-      container.appendChild(clone);
+      createRecipeCard(recipe);
     });
   } else {
     container.innerHTML = "<p>Aucune recette trouvée.</p>";
